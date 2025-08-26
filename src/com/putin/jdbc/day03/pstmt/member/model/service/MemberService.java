25a2f7c29c86b4c7fe822b20908283e721bf8956
dbsdn <dbsdn@yunwoo>
2025-08-26 17:46:09 +0900

    [ADD] 회원관리프로그램 preparedStatement, Service, Singleton 적용
    1. 입력값을 검증하여 SqlInjection 방어할 수 있는
    PreparedStatement 적용하여 JDBC 프로그래밍 완료
    2. DAO의 역할을 분할하기 위해서 Service 클래스 생성, 서비스 클래스에서는 DBMS연결 생성하여 DAO에 넘겨줌
    - DAO는 코드가 줄어들게 됨
    3. 연결을 만들어주는 코드를 JDBCTemplate 클래스에 이동 후 JDBCTemplate에는 Singleton 패턴을
    적용하여 생성된 객체를 재사용하게 함.
    4. rsetToMember() 메소드를 이용해서 후처리 하는 부분을 재사용함.

---
+++ src/com/putin/jdbc/day03/pstmt/member/model/service/MemberService.java
@@ -0,0 +1,84 @@
+package com.putin.jdbc.day03.pstmt.member.model.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.putin.jdbc.day03.pstmt.member.common.JDBCTemplate;
+import com.putin.jdbc.day03.pstmt.member.model.dao.MemberDAO;
+import com.putin.jdbc.day03.pstmt.member.model.vo.Member;
+
+public class MemberService {
+	
+	private JDBCTemplate jdbcTemplate;
+	private MemberDAO mDao;
+	
+	public MemberService() {
+		// JDBCTemplate 싱글톤으로 객체 생성
+		jdbcTemplate = JDBCTemplate.getInstance();
+		mDao = new MemberDAO();
+	}
+	
+	public List<Member> selectList() {
+		// 생성된 연결을 DAO에 전달
+		List<Member> mList = null;
+		try {
+			// 서비스에서 연결 생성
+			Connection conn = jdbcTemplate.getConnection();
+			mList = mDao.selectList(conn);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return mList;
+	}
+
+	public Member selectOneById(String memberId) {
+		Member member = null;
+		try {
+			Connection conn = jdbcTemplate.getConnection();
+			member = mDao.selectOneById(memberId, conn);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return member;
+	}
+	// 회원가입
+	public int insertMember(Member member) {
+		Connection conn = jdbcTemplate.getConnection();
+		int result = 0;
+		try {
+			result = mDao.insertMember(member,conn);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+	// 회원 정보 수정
+	public int updateMember(Member member) {
+		Connection conn = jdbcTemplate.getConnection();
+		int result = 0;
+		try {
+			result = mDao.updateMember(member,conn);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+	// 회원 정보 삭제
+	public int deleteMember(String memberId) {
+		Connection conn = jdbcTemplate.getConnection();
+		int result = 0;
+		try {
+			result = mDao.deleteMember(memberId,conn);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+}
---
+++ src/com/putin/jdbc/day03/pstmt/member/run/Run.java
@@ -0,0 +1,12 @@
+package com.putin.jdbc.day03.pstmt.member.run;
+
+import com.putin.jdbc.day03.pstmt.member.view.MemberView;
+
+public class Run {
+
+	public static void main(String[] args) {
+		MemberView view = new MemberView();
+		view.startProgram();
+	}
+
+}
